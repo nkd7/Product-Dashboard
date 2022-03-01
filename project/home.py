cd9e0b35a95f5d51b68a812f221e926566de7cd5
Andrew Coyle <coyle.128@osu.edu>
2022-03-01 11:39:12 -0800

    Set up virtual environment. Working on loging.

---
+++ project/home.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request
+from . import db
+from .GraphGeneration import GraphGenerator
+
+home = Blueprint('home', __name__)
+
+LeftMainGenerator = GraphGenerator('topLeft')
+CenterMainGenerator = GraphGenerator('topRight')
+RightMainGenerator = GraphGenerator('bottomLeft')
+SmallChartLeft = GraphGenerator('bottomRight')
+SmallChartRight = GraphGenerator('bottomRight')
+SmallChartCenter = GraphGenerator('bottomRight')
+
+@home.route('/', methods=["POST", "GET"])
+def home_page():
+    if request.method == "POST":  # True if a form was submitted. Otherwise, the default homepage with default charts is given
+        if 'product' not in request.form: # CASE: main chart update requested
+            LeftMainGenerator.getdata(request.form)
+            CenterMainGenerator.getdata(request.form)
+            RightMainGenerator.getdata(request.form)
+        else: # CASE: small chart update requested
+            SmallChartLeft.getdata(request.form)
+            SmallChartRight.getdata(request.form)
+            SmallChartCenter.getdata(request.form)
+
+    return render_template("ChartPage.html", page_title='KPI Analysis Dashboard', topLeft=LeftMainGenerator.generatechart(), topRight=CenterMainGenerator.generatechart(), bottomLeft=RightMainGenerator.generatechart(), bottomRight=SmallChartLeft.generatechart())
+
+
+@home.route('/charts/')
+def charts():
+    return render_template("ChartPage.html", page_title='Product Dashboard', topLeft=LeftMainGenerator.generatechart(), topRight=CenterMainGenerator.generatechart(), bottomLeft=RightMainGenerator.generatechart(), bottomRight=SmallChartLeft.generatechart())
---
+++ run.py
@@ -0,0 +1,6 @@
+from project import create_app
+
+if __name__ == '__main__':
+    flask_app = create_app()
+
+    flask_app.run(debug=True)
